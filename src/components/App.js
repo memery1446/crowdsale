dd2b5efad3a89d2fa51f951874a846b4086d1cd4
Mark Emery <markemery@Marks-MacBook-Pro.local>
2024-08-19 22:30:22 -0400

    08_19_2230

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
-import { Container } from 'react-bootstrap';
+import { useEffect, useState } from 'react'
+import { Container } from 'react-bootstrap'
+import { ethers } from 'ethers'
 
 //components
 import Navigation from './Navigation';
@@ -7,9 +9,32 @@ import Navigation from './Navigation';
 
 function App() {
 
+    const [account, setAccount] = useState(null)
+
+    // account -> variable of current acct value
+    // setAccount -> function to update account value
+    // null is a default value
+
+    const loadBlockchainData = async () => {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      console.log(provider)
+
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+      const account = ethers.utils.getAddress(accounts[0])
+      console.log(account)
+
+
+        //add to state
+    }
+
+    useEffect(() => {
+      loadBlockchainData()
+    });
+
   return(
   <Container>
   <Navigation />
+  {/* {read from state */}}
   </Container>    
   )
 }
