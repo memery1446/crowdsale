b6fae12ba6513736372eda3db293dd72abb3b17b
Mark Emery <markemery@Marks-MacBook-Pro.local>
2024-08-20 11:11:37 -0400

    08_20_1111

--- src/components/Buy.js
+++ src/components/Buy.js
@@ -1,30 +1,52 @@
-
-import Form from 'react-bootstrap/Form'; //1
-import Button from 'react-bootstrap/Button';//2
-import Row from 'react-bootstrap/Row'; //3
-import Col from 'react-bootstrap/Col'; //4
-import Spinner from 'react-bootstrap/Spinner'; //5
+import { useState } from 'react';
+import Form from 'react-bootstrap/Form'; 
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row'; 
+import Col from 'react-bootstrap/Col'; 
+import Spinner from 'react-bootstrap/Spinner'; 
+import { ethers } from 'ethers' 
 
 
 const Buy = ({ provider, price, crowdsale, setIsLoading }) => {
+    const [amount, setAmount] = useState('0')
+    const [isWaiting, setIsWaiting] = useState(false)
 
     const buyHandler = async (e) => {
         e.preventDefault()
-        console.log("buying tokens...")
-    }
-  
+        setIsWaiting(true)
 
+        try {
+        const signer = await provider.getSigner()
+
+        //calc eth needed
+        const value = ethers.utils.parseUnits((amount * price).toString(), 'ether')
+        const formattedAmount = ethers.utils.parseUnits(amount.toString(), 'ether')
+
+
+        const transaction = await crowdsale.connect(signer).buyTokens(formattedAmount, { value: value })
+        await transaction.wait()
+
+        } catch {
+        window.alert('User rejected or transaction reverted')
+        }
+        setIsLoading(true)
+  
+    }
     return (
         <Form onSubmit={buyHandler} style={{ maxWidth: '800px', margin: '50px auto' }}>
             <Form.Group as={Row}>
                 <Col>
-                    <Form.Control type="number" placeholder="Enter amount" />
+                    <Form.Control type="number" placeholder="Enter amount" onChange={(e) => setAmount(e.target.value)}/>
             
                     </Col>
                     <Col className='text-center'>
+                        {isWaiting ? (
+                        <Spinner animation="border"/>
+                        ) : (
                         <Button variant="primary" type="submit" style={{ width: '100%' }}>
                             Buy Tokens
-                        </Button>
+                            </Button>
+                         )}
                 </Col>
             </Form.Group>
         </Form>
